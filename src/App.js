77cb724cbf3aedba5ff766470f826109841e76ab
MarwaShehata <engmarwaadel95@gmail.com>
2022-03-21 11:56:13 +0200

    get change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+
 import './App.css';
+//import './App.scss';
+import Login from './components/auth/loginComponent/Login';
 
 function App() {
   return (
     <div className="App">
-     
+     blaaaaaah
+     <Login></Login>
     </div>
   );
 }
-
 export default App;

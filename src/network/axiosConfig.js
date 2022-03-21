e7202b6eec55633e3662370585fe10a42cdebcbc
Abram Boutros <abramboutros96@gmail.com>
2022-03-21 16:57:34 +0200

    added axios config file

---
+++ src/network/axiosConfig.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+	baseURL: "https://fakestoreapi.com",
+
+	params: {
+		api_key: "dasdjad54a5sd",
+	},
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+	function (config) {
+		// Do something before request is sent
+		console.log(config);
+		config.headers["Authorization"] = "djsad5s4a3d543sd4as5da6sd4";
+		config.params["test"] = "test";
+		return config;
+	},
+	function (error) {
+		// Do something with request error
+		return Promise.reject(error);
+	}
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+	function (response) {
+		// Any status code that lie within the range of 2xx cause this function to trigger
+		// Do something with response data
+		console.log(response);
+		return response;
+	},
+	function (error) {
+		// Any status codes that falls outside the range of 2xx cause this function to trigger
+		// Do something with response error
+		// Show ERROR Handler Message
+		return Promise.reject(error);
+	}
+);

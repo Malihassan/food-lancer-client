279196037e206aaa8f06586e36b2ea0c3b90ed60
Abram Boutros <abramboutros96@gmail.com>
2022-03-31 12:15:34 +0200

    pulling

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
 				{/*
 				dynamic routing example
 			<Route path="users" element={<Users users={users} />} /> */}
-						<Route path="/orderHistory" element={<OrderHistory />} />
+				<Route path="/orderHistory" element={<OrderHistory />} />
 				<Route path="*" element={<Navigate replace to="/home" />} />
 			</Routes>
 			<Footer />

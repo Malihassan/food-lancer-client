5aaea2aaca1bbd2bcdca4023cf2c41c7e40e8ae2
MarwaShehata <engmarwaadel95@gmail.com>
2022-04-15 04:26:31 +0200

    add username

--- src/pages/buyerHome/BuyerHome.js
+++ src/pages/buyerHome/BuyerHome.js
@@ -1,5 +1,5 @@
 import classes from "./buyerHome.module.scss";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useCallback  } from "react";
 import { IoFilterOutline } from "react-icons/io5";
 import { Rating } from "react-simple-star-rating";
 import BuyerProductCard from "./../../components/shared/buyerProductCard/BuyerProductCard";
@@ -9,120 +9,121 @@ import ReactPaginate from "react-paginate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import OffCanvas from "./../../components/shared/offCanvas/OffCanvas";
 import {
-	faCircleArrowLeft,
-	faCircleArrowRight,
+  faCircleArrowLeft,
+  faCircleArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
- import Box from "@mui/material/Box";
- import Slider from "@mui/material/Slider";
-/* function valuetext(value) {
-  //console.log(value);
-  return `${value}°C`;
-} */
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+import debounce from 'lodash.debounce';
 function BuyerHome() {
-  const { sendRequest,hasError} = useFetch();
-  const [products,setProducts] = useState([])
-  const [totalPages,setTotalPages]=useState("")
-  const [page,setPage]=useState(1)
-  const [allCategory,setAllCategory] = useState()
-  const [categoryId,setCategory] = useState()
+  
+  const { sendRequest, hasError } = useFetch();
+  const [products, setProducts] = useState([]);
+  const [totalPages, setTotalPages] = useState("");
+  const [page, setPage] = useState(1);
+  const [allCategory, setAllCategory] = useState();
+  const [categoryId, setCategory] = useState([]);
+  //const [categoryIdArr,setCategoryArr] = useState()
+  const [favs, setFavs] = useState([]);
+  const handleFavClick = (product, favStatus) => {
+    const addFav = (res) => {
+      setFavs(res.data);
+    };
+    const removeFav = (res) => {
+      setFavs(res.data);
+      console.log(res);
+    };
+    if (!favStatus) {
+      sendRequest(
+        {
+          url: `buyer/product/favs`,
+          method: "POST",
+          body: {
+            id: product._id,
+          },
+        },
+        addFav
+      );
+    } else {
+      sendRequest(
+        {
+          url: `buyer/product/favs`,
+          method: "DELETE",
+          body: {
+            id: product._id,
+          },
+        },
+        removeFav
+      );
+    }
+  };
 
-	const [favs, setFavs] = useState([]);
+  const renderProducts = () => {
+    const lockup = {};
+    for (let fav of favs) {
+      lockup[fav._id] = fav._id;
+    }
+    let list = products?.map((prd) => {
+      let fav = false;
+      if (lockup[prd._id]) fav = true;
+      return (
+        <div
+          key={prd._id}
+          className={`col-xl-4 col-lg-6 col-md-6 col-sm-8 ${classes.colsDesign}`}
+        >
+          <BuyerProductCard
+            handleFavClick={handleFavClick}
+            product={prd}
+            fav={fav}
+          />
+        </div>
+      );
+    });
 
-
-	const handleFavClick = (product, favStatus) => {
-		const addFav = (res) => {
-			setFavs(res.data);
-		};
-		const removeFav = (res) => {
-			setFavs(res.data);
-			console.log(res);
-		};
-		if (!favStatus) {
-			sendRequest(
-				{
-					url: `buyer/product/favs`,
-					method: "POST",
-					body: {
-						id: product._id,
-					},
-				},
-				addFav
-			);
-		} else {
-			sendRequest(
-				{
-					url: `buyer/product/favs`,
-					method: "DELETE",
-					body: {
-						id: product._id,
-					},
-				},
-				removeFav
-			);
-		}
-	};
-
-	const renderProducts = () => {
-		const lockup = {};
-		for (let fav of favs) {
-			lockup[fav._id] = fav._id;
-		}
-		let list = products.map((prd) => {
-			let fav = false;
-			if (lockup[prd._id]) fav = true;
-			return (
-				<div key={prd._id} className={`col-xl-4 col-lg-6 col-md-6 col-sm-8 ${classes.colsDesign}`}>
-					<BuyerProductCard
-						handleFavClick={handleFavClick}
-						product={prd}
-						fav={fav}
-					/>
-				</div>
-			);
-		});
-
-		return list;
-	};
-
- 
-const rateArr= [4.5,4.0,3.5,3.0,2.5]
-  const [priceValue, setPriceValue] = useState([40, 400]);
+    return list;
+  };
+  const rateArr = [4.5, 4.0, 3.5, 3.0,2.5,0];
+  const [priceValue, setPriceValue] = useState([40, 1000]);
   const [rateValue, setRateValue] = useState(0);
-  //const [favs, setFavs] = useState([]);
   ////********************************OFF CANVAS******************************************////
   const [toggleCanvas, setToggleCanvas] = useState(false);
   const toggleCanvasHandler = () => {
     setToggleCanvas(!toggleCanvas);
   };
-////********************************PAGINITION******************************************////
+  ////********************************PAGINITION******************************************////
   const handelPageClick = async (data) => {
     let currentPage = data.selected + 1;
-   setPage(currentPage);
+    setPage(currentPage);
   };
- ////********************************PRODUCT REQUEST******************************************////
-  
   ////********************************FILTER******************************************////
-  const handleCategoryChange=(e)=> {
+  const handleCategoryChange = (e) => {
+    console.log(e.target.value);
     if (e.target.checked) {
-      console.log(e.target.checked);
       const target = e.target.value;
-      setCategory(target)
-      return
+     console.log(typeof(target));
+      setCategory([...categoryId,target]);
+      return;
     }
-   setCategory()
-  }
+    categoryId.pop(e.target.value)
+    setCategory([...categoryId]);
+  };
   const handlePriceChange = (event, newValue) => {
     console.log(event);
     setPriceValue(newValue);
   };
-  const handleRatingsChange = (e)=>{
+  const debouncedChangeHandler = useCallback(
+    debounce(handlePriceChange, 500)
+  , [handlePriceChange]);
+
+  const handleRatingsChange = (e) => {
     if (e.target.checked) {
       const target = e.target.value;
-      setRateValue(target)
-      return
+      console.log(target);
+      setRateValue(target);
+      return;
     }
-    setRateValue()
-  }
+    setRateValue();
+  };
   useEffect(() => {
     function getAllCategry(res) {
       if (res.status === 200) {
@@ -139,189 +140,197 @@ const rateArr= [4.5,4.0,3.5,3.0,2.5]
     );
   }, [sendRequest]);
   useEffect(() => {
-    function  getAllProduct(res) {
+    function getAllProduct(res) {
+      console.log(categoryId);
       console.log(res);
-       setProducts(res?.data.docs)
-      setTotalPages(res.data.totalPages)
-    } 
+      setProducts(res?.data.docs);
+      setTotalPages(res.data.totalPages);
+    }
     function getFavs(res) {
-			setFavs(res.data);
-		}
+      setFavs(res.data);
+    }
     sendRequest(
       {
         url: `buyer/product/allProducts`,
         method: "GET",
-        params: {page,categoryId,min:priceValue[0],max:priceValue[1],rate:rateValue},
+        params: {
+          page,
+          categoryId:categoryId?categoryId:[],
+          min: priceValue[0],
+          max: priceValue[1],
+          rate: rateValue,
+        },
       },
       getAllProduct
     );
     sendRequest(
-			{
-				url: `buyer/product/favs`,
-				method: "GET",
-			},
-			getFavs
-		);
-  }, [page,sendRequest,categoryId,priceValue,rateValue]);
- 
+      {
+        url: `buyer/product/favs`,
+        method: "GET",
+      },
+      getFavs
+    );
+  }, [page, sendRequest, categoryId, priceValue, rateValue]);
+
   return (
     <>
-     <div className={`${classes.homeBody} `}>
-    <div  className={`  d-flex justify-content-end container mb-2 pt-2 pe-5 `}>
-     <button
-          className="btn mt-2 px-4" style={{backgroundColor:"#091b29",color:'#f8f8f7'}}
-          onClick={toggleCanvasHandler}
-          type="button"
+      <div className={`${classes.homeBody} `}>
+        <div
+          className={`  d-flex justify-content-end container mb-2 pt-2 pe-5 `}
         >
-          <IoFilterOutline className="me-3" style={{color:"#f8f8f7" ,fontSize:"25px"}}/>
-          Filter
-        </button>
+          <button
+            className="btn mt-2 px-4"
+            style={{ backgroundColor: "#091b29", color: "#f8f8f7" }}
+            onClick={toggleCanvasHandler}
+            type="button"
+          >
+            <IoFilterOutline
+              className="me-3"
+              style={{ color: "#f8f8f7", fontSize: "25px" }}
+            />
+            Filter
+          </button>
         </div>
         <OffCanvas
-        className={``}
-        toggleCanvas={toggleCanvas}
-        toggleCanvasHandler={toggleCanvasHandler}
-        placement={"end"}
-        name={"Filter Product"}
-        title="Filter Product"
-      >
-        <div className="mt-3">
-          <h4 className="title mt-2" style={{ color: "#091b29" }}>
-            Category
-          </h4>
-          <div className="list-group" >
-           
-            {allCategory?.map((category) => {
-                    return (
-                      <label className="list-group-item border-0" style={{ backgroundColor: "#fff" }}>
-              <input
-              
-              key={category._id} 
-                className="form-check-input me-1"
-                //checked={checked}
-                onChange={handleCategoryChange}
-                type="checkbox"
-                value={category._id}
-              />
-              {category.name}
-            </label>
-                    );
-                  })}
+          className={``}
+          toggleCanvas={toggleCanvas}
+          toggleCanvasHandler={toggleCanvasHandler}
+          placement={"end"}
+          name={"Filter Product"}
+          title="Filter Product"
+        >
+          <div className="mt-3">
+            <h4 className="title mt-2" style={{ color: "#091b29" }}>
+              Category
+            </h4>
+            <div className="list-group">
+              {allCategory?.map((category) => {
+                return (
+                  <label
+                    className="list-group-item border-0"
+                    style={{ backgroundColor: "#fff" }}
+                  >
+                    <input
+                      key={category._id}
+                      className="form-check-input me-1"
+                      //checked={checked}
+                      onChange={handleCategoryChange}
+                      type="checkbox"
+                      value={category._id}
+                    />
+                    {category.name}
+                  </label>
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div className="mt-3">
-        <h4 className="title mt-2" style={{ color: "#091b29" }}>
-            Price
-          </h4>
-      <div className="list-group ">
-     <label className="list-group-item border-0 " style={{ backgroundColor: "#fff" }}>
-     <Box className="me-1 " sx={{ width: 300 }}>
-      <Slider
-       // getAriaLabel={() => 'Temperature range'}
-       className={`${classes.slider}`}
-        value={priceValue}
-        onChange={handlePriceChange}
-        valueLabelDisplay="auto"
-        min={0}
-  max={400}
- // color="secondary"
-       // getAriaValueText={valuetext}
-      />
-    </Box>
-     </label>
-      </div>
-        </div>
-        <div className="mt-3">
-          <h4 className="title mt-2" style={{ color: "#091b29" }}>
-            Ratings
-          </h4>
-          <div className="list-group ">
-           
-           {rateArr?.map((rate) => {
-                    return (
-                      <label className="list-group-item border-0 " style={{ backgroundColor: "#fff" }}>
-              <input
-              key={rate._id} 
-                className="form-check-input me-1"
-                onChange={handleRatingsChange}
-                type="checkbox"
-                value={rate}
+          <div className="mt-3">
+            <h4 className="title mt-2" style={{ color: "#091b29" }}>
+              Price
+            </h4>
+            <div className="list-group ">
+              <label
+                className="list-group-item border-0 "
+                style={{ backgroundColor: "#fff" }}
+              >
+                <Box className="me-1 " sx={{ width: 300 }}>
+                  <Slider
+                    // getAriaLabel={() => 'Temperature range'}
+                    className={`${classes.slider}`}
+                    value={priceValue}
+                    onChange={debouncedChangeHandler} 
+                    valueLabelDisplay="auto"
+                    min={0}
+                    max={1000}
+                    // color="secondary"
+                    // getAriaValueText={valuetext}
+                  />
+                </Box>
+              </label>
+            </div>
+          </div>
+          <div className="mt-3">
+            <h4 className="title mt-2" style={{ color: "#091b29" }}>
+              Ratings
+            </h4>
+            <div className="list-group ">
+              {rateArr?.map((rate) => {
+                return (
+                  <label
+                    className="list-group-item border-0 "
+                    style={{ backgroundColor: "#fff" }}
+                  >
+                    <input
+                      key={rate._id}
+                      className={`form-check-input me-1 ${classes.squareRadio}`}
+                      onChange={handleRatingsChange}
+                      type="radio"
+                      name="flexRadioDefault"
+                      value={rate}
+                    />
+                    {rate.toFixed(1)}&up
+                    <Rating
+                      className={`${classes.stars} mx-2 pb-1`}
+                      transition
+                      readonly
+                      ratingValue={rate * 20}
+                      allowHalfIcon
+                      size={20}
+                      fillColorArray={[
+                        "red",
+                        "red",
+                        "red",
+                        "red",
+                        "orange",
+                        "orange",
+                        "orange",
+                        "yellow",
+                        "yellow",
+                        "yellow",
+                      ]}
+                    />
+                  </label>
+                  
+                );
+              })}
+            </div>
+          </div>
+        </OffCanvas>
+        <div className={`${classes.container} container  my-0`}>
+          {(hasError?.error === "Product not found !" ||
+            products === undefined) && <Empty />}
+          {!hasError && (
+            <div>
+              <div
+                className={`row justify-content-lg-start justify-content-md-center justify-content-sm-center `}
+              >
+                {renderProducts()}
+              </div>
+              <ReactPaginate
+                previousLabel={<FontAwesomeIcon icon={faCircleArrowLeft} />}
+                nextLabel={<FontAwesomeIcon icon={faCircleArrowRight} />}
+                breakLabel={"..."}
+                pageCount={totalPages}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={3}
+                onPageChange={handelPageClick}
+                containerClassName={`${classes.paginationContainer} pagination pb-3 justify-content-center`}
+                pageClassName={"page-item px-2 py-1"}
+                pageLinkClassName={`page-link ${classes.pageLink}`}
+                previousClassName={`page-item `}
+                previousLinkClassName={`page-link ${classes.pageItem}`}
+                nextClassName={"page-item"}
+                nextLinkClassName={`page-link ${classes.pageItem}`}
+                breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                activeClassName={`${classes.active}`}
               />
-              {rate.toFixed(1)}&up
-              <Rating
-					className={`${classes.stars} mx-2  pb-1`}
-					transition
-					readonly
-					ratingValue={rate*20}
-					allowHalfIcon
-					size={20}
-					fillColorArray={[
-						"red",
-						"red",
-						"red",
-						"red",
-						"orange",
-						"orange",
-						"orange",
-						"yellow",
-						"yellow",
-						"yellow",
-					]}
-				/>
-            </label>
-                    );
-                    
-                  })}
-           
-        
-        <div>
-       
+            </div>
+          )}
         </div>
-        
-                
-         
-        </div>
-        </div>
-      </OffCanvas>
-    <div className={`${classes.container} container  my-0`}>
-    {(hasError?.error === "Product not found !" || products.length===0) && <Empty />}  
-     {!hasError && ( 
-      
-    <div>
-       
-          <div className={`row justify-content-lg-start justify-content-md-center justify-content-sm-center `}>
-          {renderProducts()}
-        </div>
-          <ReactPaginate
-          previousLabel={<FontAwesomeIcon icon={faCircleArrowLeft} />}
-          nextLabel={<FontAwesomeIcon icon={faCircleArrowRight} />}
-          breakLabel={"..."}
-          pageCount={totalPages} 
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={3}
-          onPageChange={handelPageClick} 
-           containerClassName={`${classes.paginationContainer} pagination pb-3 justify-content-center`}
-          pageClassName={"page-item px-2 py-1"}
-          pageLinkClassName={`page-link ${classes.pageLink}`}
-          previousClassName={`page-item `}
-          previousLinkClassName={`page-link ${classes.pageItem}`}
-          nextClassName={"page-item"}
-          nextLinkClassName={`page-link ${classes.pageItem}`}
-          breakClassName={"page-item"}
-          breakLinkClassName={"page-link"}
-          activeClassName={`${classes.active}`} 
-        />
-    </div>
-  
-     )} 
-       
-     
-    </div>
-    </div>
-    
-    
+      </div>
     </>
-  )
+  );
 }
 
 export default BuyerHome;
